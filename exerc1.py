cd5cf1b114da5d4e79453d4ad39754d0ae08148c
Vitor Miguel <vitor2021miguel@gmail.com>
2025-07-24 15:18:23 -0300

    Update exerc1.py
    Criei uma descrição do exercício

--- exerc1.py
+++ exerc1.py
@@ -1,3 +1,6 @@
+#Este programa solicita ao usuário que informe o preço de um produto e o percentual de desconto desejado. Com base nesses dados, ele calcula o valor do desconto e o preço final do produto após o abatimento. 
+#Em seguida, exibe todas as informações na tela de forma detalhada, incluindo o preço original, o percentual de desconto, o valor do desconto e o valor final a ser pago.
+
 preco = float(input('Digite o preço do produto: '))
 percentual = float(input('Digite o percentual de desconto: '))
 desconto = preco * (percentual / 100)
